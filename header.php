d0da620ca2a08c8c8ca9816641fd95bd2964f4e1
Luiz Pedro <luizpsmoreira@gmail.com>
2020-05-01 17:06:02 -0300

    add top header

--- header.php
+++ header.php
@@ -6,4 +6,24 @@
     <?php wp_head();?>
 </head>
 
-<body>
+<body <?php body_class();?>>
+    <header>
+        <div class="top_header">
+            <div class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="collapse navbar-collapse">
+                        <?php 
+                        if (has_nav_menu('top')) {
+                            wp_nav_menu(array(
+                                'theme_location' => 'top',
+                                'container' => false,
+                                'fallback_cb' => false,
+                                'menu_class' => 'nav navbar-nav'
+                            ));
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>

c0be54f0016634d923f9e58ddf1e3de04d31d318
Luiz Pedro <luizpsmoreira@gmail.com>
2020-05-02 19:30:26 -0300

    add home

--- index.php
+++ index.php
@@ -4,7 +4,21 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-8 maincontent">
-                main
+                <?php if(have_posts()): ?>
+                <?php while(have_posts()):?>
+                <?php the_post();?>
+                <?php get_template_part('template-parts/post');?>
+                <?php endwhile;?>
+                <div class="pag">
+                    <div class="previous_pag">
+                        <?php previous_posts_link('< Anterior')?>
+                    </div>
+                    <div class="next_pag">
+                        <?php next_posts_link('Proxima >')?>
+                    </div>
+                    <div style="clear:both"></div>
+                </div>
+                <?php endif;?>
             </div>
             <?php get_sidebar();?>
         </div>

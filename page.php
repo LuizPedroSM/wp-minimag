c935914e4e2f06f296bb34c1ae0693371ce4c743
Luiz Pedro <luizpsmoreira@gmail.com>
2020-05-02 23:37:13 -0300

    add Single and Page

---
+++ page.php
@@ -0,0 +1,24 @@
+<?php get_header();?>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 maincontent">
+                <?php if(have_posts()): ?>
+                <?php while(have_posts()):?>
+                <?php the_post();?>
+
+                <div class="post_title post_title_single">
+                    <h1><?php the_title();?></h1>
+                </div>
+                <div class="post_content"><?php the_content();?></div>
+
+                <?php endwhile;?>
+
+                <?php endif;?>
+            </div>
+            <?php get_sidebar();?>
+        </div>
+    </div>
+</section>
+<?php get_footer();?>

105aa51072ff7399578fb9c89c186779bae252bb
Luiz Pedro <luizpsmoreira@gmail.com>
2020-05-03 17:49:41 -0300

    add archive, category and search

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
-<form action="/" method="get">
+<form action="<?php site_url();?>" method="get">
     <input type="text" name="s" class="search_input" placeholder="Pesquisa" value="<?php the_search_query();?>" />
     <input type="submit" class="search_submit" value="Pesquisar">
 </form>

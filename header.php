5430e94495d7e65f2aa278df8c84c6ca18f470fc
Luiz Pedro <luizpsmoreira@gmail.com>
2020-05-04 19:28:50 -0300

    add additional config template

--- header.php
+++ header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
+<html <?php language_attributes();?>>
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php bloginfo('charset');?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <?php wp_head();?>
 </head>

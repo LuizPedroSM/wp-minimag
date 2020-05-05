81059ceff73d3e753d87fde304d91aab6b6cc71c
Luiz Pedro <luizpsmoreira@gmail.com>
2020-05-05 00:08:42 -0300

    add Number Pagination

--- index.php
+++ index.php
@@ -10,12 +10,23 @@
                 <?php get_template_part('template-parts/post');?>
                 <?php endwhile;?>
                 <div class="pag">
-                    <div class="previous_pag">
-                        <?php previous_posts_link('< Anterior')?>
-                    </div>
-                    <div class="next_pag">
-                        <?php next_posts_link('Proxima >')?>
-                    </div>
+                    <?php 
+                    global $wp_query;
+                    echo paginate_links(array(
+                        'current' => max(1, get_query_var('paged')),
+                        'total' => $wp_query->max_num_pages,
+                        'show_all' => false,
+                        'end_size' => 2,
+                        'mid_size' => 2,
+                        'prev_next' => true,
+                        'prev_text' => '«',
+                        'next_text' => '»',
+                        'before_page_number' => '[',
+                        'after_page_number' => ']',
+                    ));
+                    ?>
+                    <!-- <div class="previous_pag"> <?php previous_posts_link('< Anterior')?> </div>
+                    <div class="next_pag"> <?php next_posts_link('Proxima >')?> </div> -->
                     <div style="clear:both"></div>
                 </div>
                 <?php endif;?>
